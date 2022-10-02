ee1aa63f1227349a44aa8b85d5ff257bc59b2cdb
CAPTIONH <95747644+CAPTIONH@users.noreply.github.com>
2022-10-02 21:24:02 +0800

    Update util.py

--- finetune/util.py
+++ finetune/util.py
@@ -62,3 +62,33 @@ def load_state_with_same_shape(model, weights):
     }
     logging.info("Loading weights:" + ', '.join(filtered_weights.keys()))
     return filtered_weights
+
+def get_iso_label(res):
+    def convert(x):
+        return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33][x]
+    return np.array(list(map(convert, res)))
+def calculate_shape_IoU(pred_np, seg_np, cat):
+    # pred_np [N,]          --smoothed segmentation label for each point in a point cloud
+    # seg_np  [N,]          --true segmentation label for each point in a point cloud
+    # cat   str             --category( 'u' or 'l')
+
+    if cat == 'u':
+        parts = get_iso_label(range(17))
+    else:
+        parts = [0]
+        parts.extend(list(range(17, 33, 1)))
+        parts = get_iso_label(parts)
+    
+    part_ious = []
+    for part in parts:
+        I = np.sum(np.logical_and(pred_np == part, seg_np == part))
+        U = np.sum(np.logical_or(pred_np == part, seg_np == part))
+        if U == 0:
+            iou = 1  # If the union of groundtruth and prediction points is empty, then count part IoU as 1
+        else:
+            iou = I / float(U)
+
+        part_ious.append(iou)
+    shape_ious = np.mean(part_ious)  # part IoU averaged
+
+    return shape_ious
